65635ad54c4c8846518105710b42edebf3c5c28a
iara.zas <123583066+iarazuska@users.noreply.github.com>
2025-01-10 19:02:41 +0100

    ejer1

--- layouts/src/components/Ejer1.js
+++ layouts/src/components/Ejer1.js
@@ -2,48 +2,37 @@ import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 
 function Ejer1() {
+
     return (
-        <Container fluid>
+        <Container>
             <Row>
-                <Col xs={12} md={4} style={{
+                <Col xs={12} md={6} lg={4} style={{
                     backgroundImage: "url('/mujer.png')",
                     backgroundSize: 'cover',
                     backgroundPosition: 'center center',
-                    minHeight: '100vh',  // Asegúrate de que cada columna tenga una altura mínima adecuada
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                    alignItems: 'center'
+                    height:'100vh'
                 }}>
-                    <h1 style={{ color: 'white', backgroundColor: 'rgba(0,0,0,0.5)' }}>Mujer</h1>
+                    <h1>mujer</h1>
                 </Col>
-                <Col xs={12} md={4} style={{
+                <Col xs={12} md={6} lg={4} style={{
                     backgroundImage: "url('/hombre.png')",
                     backgroundSize: 'cover',
                     backgroundPosition: 'center center',
-                    minHeight: '100vh',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                    alignItems: 'center'
+                    height:'100vh'
                 }}>
-                    <h1 style={{ color: 'white', backgroundColor: 'rgba(0,0,0,0.5)' }}>Hombre</h1>
+                    <h1>hombre</h1>
                 </Col>
-                <Col xs={12} md={4} style={{
+                <Col xs={12} md={6} lg={4} style={{
                     backgroundImage: "url('/niño.jpg')",
                     backgroundSize: 'cover',
                     backgroundPosition: 'center center',
-                    minHeight: '100vh',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                    alignItems: 'center'
+                    height:'100vh'
                 }}>
-                    <h1 style={{ color: 'white', backgroundColor: 'rgba(0,0,0,0.5)' }}>Niños</h1>
+                    <h1>niños</h1>
                 </Col>
             </Row>
         </Container>
-    );
+    )
 }
 
 export default Ejer1;
